23acd3abbac5acad8e5c15247a62d21ff624d13f
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-18 19:18:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 
 API_KEY = os.getenv('API_KEY')
 
-@bot.message_handler(commands=['start'])
+message_handler(commands=['start'])
 def start(message):
   bot.reply_text(message, "irukan")
   
