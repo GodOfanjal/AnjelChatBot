4907cbca461c006d3f0b7fd6abe5979b99294337
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-18 20:58:35 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import constants as keys
+from telegram.ext import *
+import responces as responce
+
+print("Bot started...")
+
+
+def start_command(update, context):
+    update.message.reply_text('irukan')
+
+
+def help_command(update, context):
+    update.message.reply_text('i can't help you')
+
+
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+    responce = R.sample_responces(text)
+
+    update.message.reply_text(responce)
+
+
+def error(update, context):
