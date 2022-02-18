09c9f55372d7622dce660f6529082505178b1abd
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-02-18 19:48:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 
 API_KEY = os.getenv('API_KEY')
 
-@bot.on_message(filters.command(["start", f"start@{U}"]))
+@luna.on_message(filters.command(["start", f"start@{U}"]))
 async def repo(_, message):
     await message.reply_text("irukan")    
 
